176225be76d56a7393a2442d22251ccbc857c8cf
AlmeidaStor <regianealmeidastor@gmail.com>
2022-03-27 20:20:11 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/sistemahoteleiro/SistemaHoteleiro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sistemahoteleiro;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author Stor
+ */
+public class SistemaHoteleiro {
+    void main(String[] args) {
+    NovoCadastro telaPrincipal = new NovoCadastro();
+    telaPrincipal.setVisible (true);
+    }  
+}
